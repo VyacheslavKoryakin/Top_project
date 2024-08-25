d585ea2c2fc718bf03c76c9cfbc5d55d985a7078
VyacheslaKoryakin <kkvvvv02@gmail.com>
2024-08-25 19:40:30 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        for( int i =0; i<3; i++) {
-            System.out.println("Hello world!");
+        String hello= "Hello";
+        String world = "world!";
+            System.out.println(hello+" "+world);
         }
 
     }
-}
+
 
