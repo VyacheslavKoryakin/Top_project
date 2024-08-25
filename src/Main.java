cc210e56f0de75b9cc35c6704c9c27ee1ec7032d
VyacheslaKoryakin <kkvvvv02@gmail.com>
2024-08-25 19:44:31 +0300

    Deleted cycle and add variables

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String hello= "Hello";
+        String hello = "Hello";
         String world = "world!";
-            System.out.println(hello+" "+world);
-        }
-
+        System.out.println(hello + " " + world);
     }
+
+}
 
 
