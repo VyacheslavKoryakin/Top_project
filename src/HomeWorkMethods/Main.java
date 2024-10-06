652fde6023e5a0fe6c4813eaa06d9321f34bdd9a
VyacheslaKoryakin <kkvvvv02@gmail.com>
2024-10-06 22:11:58 +0300

    Перегруженные методы + проверка на пустой массив

--- src/HomeWorkMethods/Main.java
+++ src/HomeWorkMethods/Main.java
@@ -3,6 +3,8 @@ package HomeWorkMethods;
 import java.util.Arrays;
 import java.util.Random;
 
+import static HomeWorkMethods.Methods.getFactorial;
+
 public class Main {
     public static void main(String[] args) {
         // Задание 1
@@ -27,6 +29,24 @@ public class Main {
 
         System.out.println("\n");
 
+        // Проверка метода с использованием массива из одного элемента
+        String[] string3 = {"Новая строка"};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(string3)));
+
+        System.out.println("\n");
+
+        // Проверка метода с использованием массива
+        string3 = new String[]{"строка", "Джава", " "};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(string3)));
+
+        System.out.println("\n");
+
+        // Проверка метода с использованием пустого массива
+        string3 = new String[]{};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(string3)));
+
+        System.out.println("\n");
+
 
         int number2 = 12;
         System.out.println(Arrays.toString(Methods.cornerValuesArray(1, 17, number2, -9, 88)));
@@ -34,8 +54,53 @@ public class Main {
 
         System.out.println("\n");
 
+        // Проверка метода с испольванием массива с положительными числами
+        int[] number3 = {12, 18, 1, 2, 3, 100};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(number3)));
+
+
+        System.out.println("\n");
+
+
+        // Проверка метода с испольванием массива с положительными числами
+        number3 = new int[]{-1, -18, -21, -2, -43, -100};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(number3)));
+
+
+        System.out.println("\n");
+
+        // Проверка метода с испольванием пустого массива
+        number3 = new int[]{};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(number3)));
+
+
+        System.out.println("\n");
 
         char symbol = 'a';
         System.out.println(Arrays.toString(Methods.cornerValuesArray('g', 'S', '1', 'J', symbol, 'b')));
+
+        System.out.println("\n");
+
+        // Проверка метода с использованием массива с одним элементом
+        char[] symbol2 = {'B'};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(symbol2)));
+
+        System.out.println("\n");
+
+        // Проверка метода с использованием массива
+        symbol2 = new char[]{'B', '1', 'k'};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(symbol2)));
+
+        System.out.println("\n");
+
+        // Проверка метода с использованием пустого массива
+        symbol2 = new char[]{};
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(symbol2)));
+
+        System.out.println("\n");
+
+
+        int numberForFactorial = 4;
+        System.out.println(getFactorial(numberForFactorial));
     }
 }
--- src/HomeWorkMethods/Methods.java
+++ src/HomeWorkMethods/Methods.java
@@ -25,7 +25,7 @@ public class Methods {
     }
 
     /**
-     * Метод принимает строки и
+     * Метод принимает строки или массив строк и
      * возвращает массивом самую длинную и самую короткую строки
      *
      * @param string String
@@ -36,55 +36,61 @@ public class Methods {
         int minStringLength = Integer.MAX_VALUE;
         String maxStringInArray = "";
         String minStringInArray = "";
+        if (string.length == 0) {
+            printArrayLengthError();
+            return string;
+        } else {
+            for (String s : string) {
+                if (s.length() < minStringLength) {
+                    minStringLength = s.length();
+                    minStringInArray = s;
+                }
+                if (s.length() > maxStringLength) {
+                    maxStringLength = s.length();
+                    maxStringInArray = s;
+                }
+            }
 
-        for (String s : string) {
-            if (s.length() < minStringLength) {
-                minStringLength = s.length();
-                minStringInArray = s;
-            }
-            if (s.length() > maxStringLength) {
-                maxStringLength = s.length();
-                maxStringInArray = s;
-            }
+            String[] resultArray = {maxStringInArray, minStringInArray};
+            return resultArray;
         }
-
-        String[] resultArray = {maxStringInArray, minStringInArray};
-        return resultArray;
-
     }
 
     /**
-     * Метод принимает целые числа и
+     * Метод принимает целые числа или массив целых чисел и
      * возвращает массивом наибольшее и наименьшее числа
      *
-     * @param number int
+     * @param number int или int[]
      * @return array int
      */
     static int[] cornerValuesArray(int... number) {
         int maxIntValue = Integer.MIN_VALUE;
         int minIntValue = Integer.MAX_VALUE;
+        if (number.length == 0) {
+            printArrayLengthError();
+            return number;
+        } else {
+            for (int i : number) {
+                if (i < minIntValue) {
+                    minIntValue = i;
+                }
+                if (i > maxIntValue) {
+                    maxIntValue = i;
+                }
+            }
 
-        for (int i : number) {
-            if (i < minIntValue) {
-                minIntValue = i;
-            }
-            if (i > maxIntValue) {
-                maxIntValue = i;
-            }
+            int[] resultArray = {maxIntValue, minIntValue};
+            return resultArray;
         }
-
-        int[] resultArray = {maxIntValue, minIntValue};
-        return resultArray;
-
 
     }
 
     /**
-     * Метод принимает символы и
+     * Метод принимает символы или массив символов и
      * возвращает массивом наибольший и наименьший символы
      *
-     * @param symbol int
-     * @return array int
+     * @param symbol char или char[]
+     * @return array char
      */
     static char[] cornerValuesArray(char... symbol) {
         int maxCharValue = Integer.MIN_VALUE;
@@ -92,20 +98,40 @@ public class Methods {
         char maxCharInArray = 0;
         char minCharInArray = 0;
 
-        for (char c : symbol) {
-            if (c < minCharValue) {
-                minCharValue = c;
-                minCharInArray = c;
+        if (symbol.length == 0) {
+            printArrayLengthError();
+            return symbol;
+        } else {
+            for (char c : symbol) {
+                if (c < minCharValue) {
+                    minCharValue = c;
+                    minCharInArray = c;
 
+                }
+                if (c > maxCharValue) {
+                    maxCharValue = c;
+                    maxCharInArray = c;
+                }
             }
-            if (c > maxCharValue) {
-                maxCharValue = c;
-                maxCharInArray = c;
-            }
+
+            char[] resultArray = {maxCharInArray, minCharInArray};
+            return resultArray;
         }
+    }
 
-        char[] resultArray = {maxCharInArray, minCharInArray};
-        return resultArray;
+    /**
+     * Медот для вывода в консоль.
+     * Метод не принимает значений.
+     */
+    static void printArrayLengthError() {
+        System.out.println("Введен пустой массив");
+    }
 
+    static int getFactorial(int number) {
+        if (number == 1) {
+            return 1;
+        } else {
+            return number * getFactorial(number - 1);
+        }
     }
 }
