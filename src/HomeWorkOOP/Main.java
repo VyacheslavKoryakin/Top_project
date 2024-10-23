5c0bc0cf15f0958660c2d9f375730dce0a92ef84
VyacheslaKoryakin <kkvvvv02@gmail.com>
2024-10-23 21:11:37 +0300

    принципы ООП 31-32

--- src/HomeWorkOOP/Main.java
+++ src/HomeWorkOOP/Main.java
@@ -2,26 +2,27 @@ package HomeWorkOOP;
 
 public class Main {
     public static void main(String[] args) {
-        Author author1 = new Author();
-        author1.setName("Андре");
-        author1.setSurname("Нортон");
+        Author author1 = new Author("Андре", "Нортон");
 
-        Book book1 = new Book();
-        book1.setAuthor(author1);
-        book1.setTitle("Саргассы в космосе");
-        book1.setYear(1956);
+        Book book1 = new Book(author1, "Саргассы в космосе", 1956);
 
-        Author author2 = new Author();
-        author2.setName("Рэй");
-        author2.setSurname("Бредбери");
+        Author author2 = new Author("Рэй", "Бредбери");
 
-        Book book2 = new Book();
-        book2.setAuthor(author2);
-        book2.setTitle("И грянул гром");
-        book2.setYear(1952);
+        Book book2 = new Book(author2, "И грянул гром", 1952);
 
 
-        System.out.println(book1.getInfo());
-        System.out.println(book2.getInfo());
+        System.out.println(author1);
+        System.out.println(author2);
+
+        System.out.println(book1);
+        System.out.println(book2);
+
+        book1.setTitle("Война и мир");
+        book1.setYear(1868);
+        book2.setAuthor(new Author("Александр", "Дюма"));
+
+        System.out.println(book1.getAuthor() + " " + book1.getTitle() + " " + book1.getYear());
+        System.out.println(book2.getAuthor() + " " + book2.getTitle() + " " + book2.getYear());
+
     }
 }
